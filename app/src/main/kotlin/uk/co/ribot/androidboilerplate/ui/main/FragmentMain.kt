c4f093190c1746d6308f86fd89cc28e1191ce9aa
Nikolay <NikolayManzhos@users.noreply.github.com>
2017-08-12 14:57:51 +0300

    Change MainFragment

--- app/src/main/kotlin/uk/co/ribot/androidboilerplate/ui/main/FragmentMain.kt
+++ app/src/main/kotlin/uk/co/ribot/androidboilerplate/ui/main/FragmentMain.kt
@@ -5,11 +5,21 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import butterknife.ButterKnife
+import butterknife.Unbinder
 import uk.co.ribot.androidboilerplate.R
 
 class FragmentMain: Fragment() {
 
+    private lateinit var unbinder:Unbinder
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_main, container, false)
+        val view = inflater?.inflate(R.layout.fragment_main, container, false) as View
+        unbinder = ButterKnife.bind(this, view)
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+
     }
 }
