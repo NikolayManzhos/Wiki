f964c13c6d91755c518a160be888fc2ddf6ea388
mishkun.lev <mishkun.lev@gmail.com>
2017-08-12 14:25:54 +0300

    parceInteractor template

--- app/src/main/kotlin/uk/co/ribot/androidboilerplate/data/ParseInteractor.kt
+++ app/src/main/kotlin/uk/co/ribot/androidboilerplate/data/ParseInteractor.kt
@@ -1,10 +1,20 @@
 package uk.co.ribot.androidboilerplate.data
 
+import rx.Single
+import uk.co.ribot.androidboilerplate.data.model.WikiPage
 import javax.inject.Inject
 
 /**
  * Created by Mishkun on 12.08.2017.
  */
-class ParseInteractor @Inject constructor(dataManager: DataManager){
-    
+class ParseInteractor @Inject constructor(private val dataManager: DataManager) {
+    fun getParsed(query: String): Single<WikiPage> {
+        return dataManager.getWiki()
+        //return Single.just(DummyData().data)
+        //.map { Jsoup.parse(it.html) }
+        //.map { Jsoup.parse(it) }
+        //.map { it.select("header-container").remove()}
+    }
+
+
 }
