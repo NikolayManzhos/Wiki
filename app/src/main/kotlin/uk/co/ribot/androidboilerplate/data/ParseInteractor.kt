fa41f62fbe06e9953219ae090d5a0f762406981f
mishkun.lev <mishkun.lev@gmail.com>
2017-08-12 14:00:28 +0300

    added jsoup

---
+++ app/src/main/kotlin/uk/co/ribot/androidboilerplate/data/ParseInteractor.kt
@@ -0,0 +1,10 @@
+package uk.co.ribot.androidboilerplate.data
+
+import javax.inject.Inject
+
+/**
+ * Created by Mishkun on 12.08.2017.
+ */
+class ParseInteractor @Inject constructor(dataManager: DataManager){
+    
+}
